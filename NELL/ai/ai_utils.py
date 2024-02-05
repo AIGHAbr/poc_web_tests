456a0254656c26d11e2c941ed2a60f90239fa8a9
sbihaiko <bihaiko@protonmail.com>
2024-02-05 17:43:12 -0300

    wip

--- NELL/ai/ai_utils.py
+++ NELL/ai/ai_utils.py
@@ -1,16 +1,29 @@
+
 import os
+import httpx
 import NELL.ai.prompts as prompts
-from langchain_openai import OpenAI
+
+api_key = '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'
+os.environ["OPENAI_API_KEY"]=api_key
 
 def generate_robot(logs):
-    api_key = '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'
-    os.environ["OPENAI_API_KEY"]=api_key
-    llm = OpenAI(api_key=api_key, model_name="gpt-4-0125-preview")
-    prompt = prompts.prompt_robot + logs
+    api_key = 'sua_chave_de_api'
+    os.environ["OPENAI_API_KEY"] = api_key
+    
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+    
+    data = {
+        "model": "gpt-4-0125-preview",  # Substitua pelo modelo de chat correto, se necessário
+        "messages": [{"role": "system", "content": "Seu prompt inicial aqui"}, {"role": "user", "content": logs}]
+    }
+    
+    response = httpx.post("https://api.openai.com/v1/chat/completions", json=data, headers=headers)
+    
+    if response.status_code == 200:
+        result = response.json()
+        return result
 
-    response = llm.generate(
-        [prompt],
-        temperature=0.1
-    )
+    raise Exception("Falha na requisição da API", response.text)
 
-    return response.generations[0][0].text
