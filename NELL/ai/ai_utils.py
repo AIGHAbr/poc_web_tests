a25bdbe63d531734cced027dcb3e8e3caab56e14
sbihaiko <bihaiko@protonmail.com>
2024-02-05 13:59:10 -0300

    wip

--- NELL/ai/ai_utils.py
+++ NELL/ai/ai_utils.py
@@ -3,13 +3,14 @@ import NELL.ai.prompts as prompts
 from langchain_openai import OpenAI
 
 def generate_robot(logs):
-    os.environ["OPENAI_API_KEY"]='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'
-    llm = OpenAI()
+    api_key = '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'
+    os.environ["OPENAI_API_KEY"]=api_key
+    llm = OpenAI(api_key=api_key, model_name="gpt-4-0125-preview")
     prompt = prompts.prompt_robot + logs
 
     response = llm.generate(
-        [prompt], 
-        temperature=0.1,
+        [prompt],
+        temperature=0.1
     )
 
     return response.generations[0][0].text
--- NELL/gui/Tabs.py
+++ NELL/gui/Tabs.py
@@ -1,4 +1,3 @@
-
 import ipywidgets as widgets
 from NELL.ai.ai_utils import generate_robot
 
@@ -9,55 +8,57 @@ from NELL.logger.LogHttpServer import Logger
 class Tabs():
     
     def __init__(self, mapping, props):
-
         self.content = widgets.Tab()
         self.tab_tests = gui.new_cell(widgets.HBox([mapping, props]), width='98%', height='400px')
         self.control_center = ControlCenter()
         self.tabs_control = gui.new_cell(self.control_center.content, width='98%', height='400px', border='0px solid white')
         self.htmlLogs = widgets.HTML()
         self.tab_event_logs = gui.new_cell(self.htmlLogs, width='98%', height='400px', scroll=True)
-        self.tab_robot = widgets.HTML()
-        # self.tab_buddy = new_cell(widgets.HTML(), width='900px', height='400px')
         
-        self.content.children = [self.tabs_control,
-                                 self.tab_event_logs, 
-                                 self.tab_tests, 
-                                 self.tab_robot]
+        # Inicializa tab_robot como Textarea
+        self.tab_robot = widgets.Textarea(
+            layout=widgets.Layout(
+                width='100%', 
+                height='400px',  # Define a altura para preencher a aba
+                overflow='auto'  # Habilita o scroll
+            )
+        )
+        
+        self.content.children = [
+            self.tabs_control,
+            self.tab_event_logs, 
+            self.tab_tests, 
+            self.tab_robot  # Atualiza esta linha para incluir o Textarea diretamente
+        ]
         
         self.content.set_title(0, 'Test Control Center')
         self.content.set_title(1, 'Event Journal')
         self.content.set_title(2, 'Quality Assurance')
-        self.content.set_title(3, 'Robot Framework') 
-        # self.content.set_title(2, 'Buddy')
+        self.content.set_title(3, 'Robot Framework')
+
+        self.last_log_sent = None
 
         Logger.add_event_logger_listener(
             lambda event, events: self.log_event(event, events))
         
-        self.content.observe(self.on_tab_change, 'Robot Framework')
+        self.content.observe(self.on_tab_change, 'selected_index')
 
 
     def log_event(self, event, events):
-
-        evets = []
+        event_list = []
         size = len(events)
         for i, evt in enumerate(reversed(events), start=1):
             j = size - i
-            evets.append(f"{j}. {str(evt)}")
+            event_list.append(f"{j}. {str(evt)}")
 
-        self.htmlLogs.value="<br/>\n".join(evets)
+        self.htmlLogs.value = "<br/>\n".join(event_list)
         print(events)
 
 
     def on_tab_change(self, change):
-        if change['new'] == 3: 
-            current_logs = "<br/>\n".join(self.tab_event_logs.children[0].value)
-            if current_logs != self.last_log_sent: 
+        if self.content.selected_index == 3:  # Checa se a aba do Robot Framework foi selecionada
+            current_logs = self.htmlLogs.value
+            if current_logs != self.last_log_sent:
                 self.last_log_sent = current_logs
                 robot_script = generate_robot(current_logs)
-                self.tab_robot.children = [widgets.Textarea(
-                                                value=robot_script, 
-                                                layout=widgets.Layout(
-                                                width='100%', 
-                                                height='100%',
-                                                border='1px solid white'
-                                            ))] 
+                self.tab_robot.value = robot_script  # Atualiza o valor do Textarea
